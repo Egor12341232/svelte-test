66d7a2d01ee455157746ab7c77651ea4528a68cf
Egor <prokofevegor65@gmail.com>
2024-02-28 09:48:45 +0500

    +layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<nav>
+    <div class="nav-container">
+    <a href="/" class="nav_logo" title="Black to Homepage">Prokofev Egor</a>
+    <div class="nav-links">
+        <a href="/about" class="link">Обомне</a>
+        <a href="/blog" class="link">Блог</a>
+        <a href="/projects" class="link">Портфолио</a>
+        <a href="/contact" class="link">Контакты</a>
+    </div>
+    </div>
+    </nav>
+    
+    <div class="container">
+    <!--Pages will be injected below-->
+    <slot></slot>
+    </div>
+    
+    <style>
+    .container {
+        max-width: 1400px;
+        margin: 50px auto;
+    }
+    </style>
