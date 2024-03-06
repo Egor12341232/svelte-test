94a177630f62de403142543ff02c9ff5695b7673
Egor <prokofevegor65@gmail.com>
2024-03-06 10:50:46 +0500

    add pages

---
+++ src/routes/blog/posts/2/+page.svelte
@@ -0,0 +1,15 @@
+<h1>My Second Blog Post</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta explicabo adipisci inventore esse quia laborum laudantium quod nisi eum amet? Facilis odio quae tenetur quaerat asperiores alias neque esse eaque.</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor animi nulla rerum ad non quidem voluptates, architecto provident ipsam quibusdam delectus? Ex, odit optio esse placeat nam officia ducimus in.</p>
+<div class="container">
+    <a href="/blog">Домой</a> 
+    <slot />
+    </div>
+    
+    <style> 
+    .container {
+    margin: 50px auto;
+    max-width: 800px;
+    padding: 0 20px;
+    }
+    </style>
